6f45b1166352deff04d48dc991a7480dd3898569
최승희 <csh1002sh@gmail.com>
2021-03-01 22:23:03 +0900

    no message

--- test.py
+++ test.py
@@ -7,18 +7,32 @@
 # a = a - 3 와 a -= 3는 같음.
 # 행 바꿀 때는 '''ㅇㅇ''' 후에 중간에 엔터해야 결과값에도 나옴.
 # a.upper 모두 대문자 / a.replace('h', 'k') 모든 h를 k
-# .format(temperature, prob) {}에 대입
+
+# .format 문자열내의 특정한 값을 변수로부터 초기화하여 동적으로 문자열을 생성
+# {} : 그대로, {{}} : 중괄호만 값 출력 x, {{{}}} : 중괄호 포함, 값 출력 / 구구단 가능
+# % 서식문자 (%s 문자열, %d 정수, %f 실수)
+# - 변수t = 50, s = 'my age %d(변수 타입)' % 변수t // 'my age 50'
+#
+
 # a.split('w') w로 구분해 나누기
+# - a = 'hello world'.split('w'), print(a) // 'hello', 'orld'
+
 # list(a) 알파벳 나누어 배열
-# print(type(a))
-# print(type(b))
-# b = a.replace( "1", "2")
 
-# format()
+# print(type('a')) // <class 'str'>, print(type(2021)) // <class 'int'>
 
-t = 25.5
-p = 60
+# .replace 문자열 내의 특정 문자 치환
+# - a = "영어는 1등이야.".replace("1", "2") // "영어는 2등이야."
 
-a = "오늘 기온{}도 이고, 비올 확률은 {}% 입니다.".format(t, p)
-print(a)
+names = ['kim', 'park', 'lee']
+for i in names:
+    print('my name is %s' % i)
+
+
+money = 10000
+s2 = 'give me %d won' % money
+print(s2)
+
+
+
 
